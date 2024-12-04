899924758fc4f5960a79ee3cd4fdc47f7ffc17e9
pensiveant <142905570003@qq.com>
2024-12-04 17:08:34 +0800

    feat: 图元案例，样式调整

--- src/Note/primitive/index.js
+++ src/Note/primitive/index.js
@@ -14,7 +14,7 @@ let vertex = `
 // 片元着色器
 let fragment = `
     void main() {
-        gl_FragColor=vec4(1.0,0.0,0.0,1.0);
+        gl_FragColor=vec4(199.0/255.0,237.0/255.0,204.0/255.0,1.0);
     }
 `;
 

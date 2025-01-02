d82b19e60c236762455583252960184e94ad81c8
pensiveant <142905570003@qq.com>
2025-01-02 10:54:43 +0800

    feat: 抽取公共方法

--- src/Note/1. hello/index.js
+++ src/Note/1. hello/index.js
@@ -1,51 +1,47 @@
-import {requestFile} from "../core/index.js"
-
+import {
+  requestFile,
+  getWebgl,
+  createShader,
+  createProgram,
+} from "../core/index.js";
 
 // 获取渲染上下文
-/** @type {HTMLCanvasElement} */
-let canvas = document.getElementById("canvas");
-let gl = canvas.getContext("webgl");
+let gl = getWebgl("canvas");
 
 // 顶点着色器
-let vertex = await requestFile('./hello.vs');
+let vertex = await requestFile("./hello.vs");
 
 // 片元着色器
-let fragment = await requestFile('./hello.fs');;
+let fragment = await requestFile("./hello.fs");
 
 // 上传代码并编译为着色器
-let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vertex);
-gl.compileShader(vertexShader);
-
-let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fragment);
-gl.compileShader(fragmentShader);
+let vertexShader = createShader(gl, "vs", vertex);
+let fragmentShader = createShader(gl, "fs", fragment);
 
 // 创建着色器程序，并链接着色器
-let program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
+let program = createProgram(gl, [vertexShader, fragmentShader]);
 
 // 创建缓冲
 let posBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-var position=[
-    -0.5,0.0, // 顶点1
-    0.5,0.0,  // 顶点2
-    0,0.5,    // 顶点3
+var position = [
+  -0.5,
+  0.0, // 顶点1
+  0.5,
+  0.0, // 顶点2
+  0,
+  0.5, // 顶点3
 ];
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position), gl.STATIC_DRAW);
 
 //#region 渲染阶段
 gl.useProgram(program);
 
-let positionLoc=gl.getAttribLocation(program,'a_position');
+let positionLoc = gl.getAttribLocation(program, "a_position");
 gl.enableVertexAttribArray(positionLoc);
-gl.bindBuffer(gl.ARRAY_BUFFER,posBuffer);
-let size=2;
-gl.vertexAttribPointer(positionLoc,size,gl.FLOAT,false,0,0);
+gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
+let size = 2;
+gl.vertexAttribPointer(positionLoc, size, gl.FLOAT, false, 0, 0);
 
 gl.drawArrays(gl.TRIANGLES, 0, 3);
 //#endregion
-
--- src/Note/core/index.js
+++ src/Note/core/index.js
@@ -1,15 +1,61 @@
 async function requestFile(filePath) {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', filePath, true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                let shaderSource = xhr.responseText;
-                resolve(shaderSource);
-            }
-        };
-        xhr.send();
-    })
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", filePath, true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        let shaderSource = xhr.responseText;
+        resolve(shaderSource);
+      }
+    };
+    xhr.send();
+  });
 }
 
-export { requestFile };
+/**
+ * 获取WebGLRenderingContext对象
+ * @param {string} id canvas元素的id
+ * @returns
+ */
+function getWebgl(id) {
+  /** @type {HTMLCanvasElement} */
+  let canvas = document.getElementById(id);
+  let gl = canvas.getContext("webgl");
+  return gl;
+}
+
+/**
+ * 创建着色器
+ * @param {WebGLRenderingContext} gl
+ * @param {'vs'|'fs'} type  着色器类型
+ * @param {string} source   着色器源码
+ * @returns
+ */
+function createShader(gl, type, source) {
+  let shaderType = {
+    vs: gl.VERTEX_SHADER,
+    fs: gl.FRAGMENT_SHADER,
+  };
+  let shader = gl.createShader(shaderType[type]);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  return shader;
+}
+
+/**
+ * 创建渲染程序
+ * @param {WebGLRenderingContext} gl
+ * @param {Array<Shader>} shaders 
+ * @returns 
+ */
+function createProgram(gl, shaders) {
+  let program = gl.createProgram();
+  for (let i = 0, len = shaders.length; i < len; i++) {
+    const item = shaders[i];
+    gl.attachShader(program, item);
+  }
+  gl.linkProgram(program);
+  return program;
+}
+
+export { requestFile, getWebgl, createShader, createProgram };
